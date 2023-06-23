160ac5cfa2d598b223800f65f6fc392ef87e0515
chanisyandia <chanisyandiafz@gmail.com>
2023-06-23 23:54:37 +0700

    final

--- lib/init.dart
+++ lib/init.dart
@@ -1,5 +1,5 @@
 class urlHTTP {
-  static String uri = "http://192.168.100.207:8080/proyek3_api/";
+  static String uri = "http://192.168.100.207git :8080/proyek3_api/";
   //static String uri = "http://172.20.10.12:8080/proyek3_api/";
   //static String uri = "http://10.0.167.94:8080/proyek3_api/";
 }
